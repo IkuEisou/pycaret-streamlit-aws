5b5e4dc0c226b838b3725bf585f72e50692410c6
yuyc <cleverforever@gmail.com>
2020-10-20 17:08:27 +0900

    add the other models

--- app.py
+++ app.py
@@ -28,70 +28,81 @@ def find(pattern, path):
     return result
 
 
+def trainModel():
+    dType = st.selectbox('Type', ['Classification', 'Regression'])
+    ds = ''
+    if(dType == 'Regression'):
+        ds = ['insurance', 'house']
+    elif(dType == 'Classification'):
+        ds = ['credit', 'bank']
+    dataset = st.selectbox('Dataset', ds)
+    targetVal = ''
+    if dataset == 'insurance':
+        targetVal = 'charges'
+    elif dataset == 'house':
+        targetVal = 'SalePrice'
+    elif dataset == 'credit':
+        targetVal = 'default'
+    elif dataset == 'bank':
+        targetVal = 'deposit'
+
+    target = st.selectbox('Target', [targetVal])
+    if st.button("作成"):
+        url = 'http://127.0.0.1:5000/automl'
+        myobj = {'target': targetVal, 'dataset': dataset}
+        chunk_size = 1024
+        'Waiting for training model...'
+        r = requests.post(url, data=myobj, stream=True)
+        content_size = int(r.headers['content-length'])
+        now = datetime.now()
+        dt_string = now.strftime("%Y%m%d%H%M%S")
+        fileName = 'best_model_'+dataset+'_'+dt_string+'.pkl'
+        with open(fileName, "wb") as pkl:
+            # Add a placeholder
+            latest_iteration = st.empty()
+            'Downloading the model...'
+            bar = st.progress(0)
+            size = 0
+            for chunk in r.iter_content(chunk_size):
+                if chunk:
+                    pkl.write(chunk)
+                    size = len(chunk)+size
+                    # Update the progress bar with each iteration.
+                    latest_iteration.text(
+                        f'Iteration {int(size/content_size)*100}')
+                    bar.progress(int(size/content_size)*100)
+                    time.sleep(0.1)
+
+        st.success(
+            'The model of predicting {} from {} has been finished'.format(target, dataset))
+
+
 def run():
     from PIL import Image
     image = Image.open(IMG_PATH+'logo.png')
     image_hospital = Image.open(IMG_PATH+'hospital.jpg')
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
     st.image(image, use_column_width=False)
-    fileName = 'best_model_insurance_20201019182828'
     add_selectbox = st.sidebar.selectbox(
         "How would you like to predict?",
-        ('Training', "Online", "Batch"))
-
-    st.sidebar.info('This app is created to predict patient hospital charges')
-    st.sidebar.success('https://www.pycaret.org')
-
+        ("Online", "Batch"))
+    # st.sidebar.info('This app is created to predict patient hospital charges')
+    # st.sidebar.success('https://milize.co.jp')
     st.sidebar.image(image_hospital)
+    st.title("Milize AutoML Demo App")
 
-    st.title("Insurance Charges Prediction App")
-    if add_selectbox == 'Training':
-        dataset = st.selectbox('Dataset', ['insurance', 'house'])
-        targetVal = ''
-        if dataset == 'insurance':
-            targetVal = 'charges'
-        elif dataset == 'house':
-            targetVal = 'SalePrice'
-        target = st.selectbox('Target', [targetVal])
-        if st.button("AutoML"):
-            url = 'http://127.0.0.1:5000/automl'
-            myobj = {'target': targetVal, 'dataset': dataset}
-            r = requests.post(url, data=myobj)
-            now = datetime.now()
-            dt_string = now.strftime("%Y%m%d%H%M%S")
-            fileName = 'best_model_'+dataset+'_'+dt_string+'.pkl'
-            with open(fileName, "wb") as pkl:
-                for chunk in r.iter_content(chunk_size=1024):
-                    if chunk:
-                        pkl.write(chunk)
-            # data = get_data(dataset)
-            # s1 = setup(data, target=targetVal, silent=True, html=False)
-            # top3 = compare_models()
-            # # finalize best model
-            # best = finalize_model(top3)
-            # now = datetime.now()
-            # dt_string = now.strftime("%Y%m%d%H%M%S")
-            # modelName = 'best_model_'+dataset+'_'+dt_string
-            # save_model(best, modelName)
-            # fileName = modelName + '.pkl'
-            # 'Starting a long computation...'
-
-            # # Add a placeholder
-            # latest_iteration = st.empty()
-            # bar = st.progress(0)
-
-            # for i in range(100):
-            #     # Update the progress bar with each iteration.
-            #     latest_iteration.text(f'Iteration {i+1}')
-            #     bar.progress(i + 1)
-            #     time.sleep(0.1)
-
-            # '...and now we\'re done!'
-            st.success(
-                'The model of predicting {} from {} has been finished'.format(target, dataset))
     models = find('*.pkl', './')
-    print(models)
-    if add_selectbox == 'Online':
+    if len(models) == 0:
+        st.warning("モデルはありません。新規作成してください。")
+        trainModel()
+    elif add_selectbox == 'Online':
         modelName = sex = st.selectbox('Model', models)
         age = st.number_input('Age', min_value=1, max_value=100, value=25)
         sex = st.selectbox('Sex', ['male', 'female'])
@@ -116,7 +127,7 @@ def run():
 
         st.success('The output is {}'.format(output))
 
-    if add_selectbox == 'Batch':
+    elif add_selectbox == 'Batch':
         modelName = sex = st.selectbox('Model', models)
         file_upload = st.file_uploader(
             "Upload csv file for predictions", type=["csv"])
