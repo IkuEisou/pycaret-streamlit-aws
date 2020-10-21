ac01c6b4bfa0a93155beb31c67a81a231af16ad9
yuyc <cleverforever@gmail.com>
2020-10-21 16:20:06 +0900

    add iris model

--- app.py
+++ app.py
@@ -9,8 +9,17 @@ import time
 import requests
 import os
 import fnmatch
+from pathlib import Path
 
 IMG_PATH = 'public/static/img/'
+MODEL_PATH = 'volume/models/'
+Features = {
+    'insurance': ['age', 'sex', 'bmi', 'children', 'smoker', 'region'],
+    'diamond': ['Carat', 'Weight', 'Cut', 'Color',
+                'Clarity', 'Polish', 'Symmetry', 'Report'],
+    'iris': ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+}
+LabelEncoded = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
 
 
 def predict(model, input_df):
@@ -28,36 +37,18 @@ def find(pattern, path):
     return result
 
 
-def trainModel():
-    dType = st.selectbox('Type', ['Classification', 'Regression'])
-    ds = ''
-    if(dType == 'Regression'):
-        ds = ['insurance', 'house']
-    elif(dType == 'Classification'):
-        ds = ['credit', 'bank']
-    dataset = st.selectbox('Dataset', ds)
-    targetVal = ''
-    if dataset == 'insurance':
-        targetVal = 'charges'
-    elif dataset == 'house':
-        targetVal = 'SalePrice'
-    elif dataset == 'credit':
-        targetVal = 'default'
-    elif dataset == 'bank':
-        targetVal = 'deposit'
-
-    target = st.selectbox('Target', [targetVal])
+def trainModel(dType, dataset, targetVal, target):
     if st.button("作成"):
         url = 'http://127.0.0.1:5000/automl'
-        myobj = {'target': targetVal, 'dataset': dataset}
+        myobj = {'type': dType, 'target': targetVal, 'dataset': dataset}
         chunk_size = 1024
         'Waiting for training model...'
         r = requests.post(url, data=myobj, stream=True)
         content_size = int(r.headers['content-length'])
         now = datetime.now()
         dt_string = now.strftime("%Y%m%d%H%M%S")
-        fileName = 'best_model_'+dataset+'_'+dt_string+'.pkl'
-        with open(fileName, "wb") as pkl:
+        fileName = dataset+'_'+dt_string+'.pkl'
+        with open(MODEL_PATH+dataset+'/'+fileName, "wb") as pkl:
             # Add a placeholder
             latest_iteration = st.empty()
             'Downloading the model...'
@@ -81,14 +72,15 @@ def run():
     from PIL import Image
     image = Image.open(IMG_PATH+'logo.png')
     image_hospital = Image.open(IMG_PATH+'hospital.jpg')
+    irisImage = Image.open(IMG_PATH+'iris-machinelearning.jpg')
     hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     st.image(image, use_column_width=False)
     add_selectbox = st.sidebar.selectbox(
         "How would you like to predict?",
@@ -97,35 +89,66 @@ def run():
     # st.sidebar.success('https://milize.co.jp')
     st.sidebar.image(image_hospital)
     st.title("Milize AutoML Demo App")
-
-    models = find('*.pkl', './')
+    dType = st.selectbox('Type', ['Regression', 'Classification'])
+    ds = ''
+    if(dType == 'Regression'):
+        ds = ['insurance']
+    elif(dType == 'Classification'):
+        ds = ['iris']
+    dataset = st.selectbox('Dataset', ds)
+    targetVal = ''
+    if dataset == 'insurance':
+        targetVal = 'charges'
+    elif dataset == 'iris':
+        targetVal = 'species'
+    Path(MODEL_PATH+dataset).mkdir(parents=True, exist_ok=True)
+    models = find('*.pkl', MODEL_PATH+dataset+'/')
+    target = st.selectbox('Target', [targetVal])
     if len(models) == 0:
         st.warning("モデルはありません。新規作成してください。")
-        trainModel()
+        trainModel(dType, dataset, targetVal, target)
     elif add_selectbox == 'Online':
-        modelName = sex = st.selectbox('Model', models)
-        age = st.number_input('Age', min_value=1, max_value=100, value=25)
-        sex = st.selectbox('Sex', ['male', 'female'])
-        bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
-        children = st.selectbox('Children', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        if st.checkbox('Smoker'):
-            smoker = 'yes'
-        else:
-            smoker = 'no'
-        region = st.selectbox(
-            'Region', ['southwest', 'northwest', 'northeast', 'southeast'])
-
+        modelName = st.selectbox('Model', models)
+        input_dict = {}
+        if dataset == 'insurance':
+            age = st.number_input('Age', min_value=1, max_value=100, value=25)
+            sex = st.selectbox('Sex', ['male', 'female'])
+            bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
+            children = st.selectbox(
+                'Children', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            if st.checkbox('Smoker'):
+                smoker = 'yes'
+            else:
+                smoker = 'no'
+            region = st.selectbox(
+                'Region', ['southwest', 'northwest', 'northeast', 'southeast'])
+            input_dict = {'age': age, 'sex': sex, 'bmi': bmi,
+                          'children': children, 'smoker': smoker, 'region': region}
+        elif dataset == 'iris':
+            sepal_length = st.number_input(
+                'sepal_length', min_value=1.0, max_value=100.0, value=5.1)
+            sepal_width = st.number_input(
+                'sepal_width', min_value=1.0, max_value=50.0, value=3.5)
+            petal_length = st.number_input(
+                'petal_length', min_value=1.0, max_value=100.0, value=1.4)
+            petal_width = st.number_input(
+                'petal_width', min_value=0.0, max_value=50.0, value=0.2)
+            input_dict = {'sepal_length': sepal_length, 'sepal_width': sepal_width,
+                          'petal_length': petal_length, 'petal_width': petal_width
+                          }
         output = ""
-
-        input_dict = {'age': age, 'sex': sex, 'bmi': bmi,
-                      'children': children, 'smoker': smoker, 'region': region}
         input_df = pd.DataFrame([input_dict])
-        model = load_model(modelName)
+
+        model = load_model(MODEL_PATH+dataset+'/'+modelName)
         if st.button("Predict"):
             output = predict(model=model, input_df=input_df)
-            output = '$' + str(output)
-
-        st.success('The output is {}'.format(output))
+            if dataset == 'insurance':
+                output = 'The charges is {}'.format('$' + str(output))
+            elif dataset == 'iris':
+                output = 'This is '+LabelEncoded[output]
+            st.success(output)
+            if dataset == 'iris':
+                st.image(irisImage, width=500)
 
     elif add_selectbox == 'Batch':
         modelName = sex = st.selectbox('Model', models)
@@ -133,12 +156,17 @@ def run():
             "Upload csv file for predictions", type=["csv"])
 
         if file_upload is not None:
+            print(file_upload)
             data = pd.read_csv(file_upload)
-            model = load_model(modelName)
+            model = load_model(MODEL_PATH+dataset+'/'+modelName)
             predictions = predict_model(estimator=model, data=data).rename(
-                columns={'Label': 'Charges'})
-            print(predictions)
+                columns={'Label': target})
+            predictions[target].replace(
+                {0: LabelEncoded[0], 1: LabelEncoded[1], 2: LabelEncoded[2]}, inplace=True)
+
             st.write(predictions)
+            if dataset == 'iris':
+                st.image(irisImage, width=500)
 
 
 if __name__ == '__main__':
